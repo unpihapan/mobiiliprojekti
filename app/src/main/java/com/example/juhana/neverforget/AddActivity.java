fa0685c1bab29c622137d1c68c777a691a428ae0
Junessi <>
2017-10-31 15:50:01 +0200

    addactivity update

--- app/src/main/java/com/example/juhana/neverforget/AddActivity.java
+++ app/src/main/java/com/example/juhana/neverforget/AddActivity.java
@@ -5,8 +5,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -23,9 +28,13 @@ public class AddActivity extends AppCompatActivity {
     ArrayList<HashMap<String, String>> card = new ArrayList<>();
    // ArrayList<HashMap<String, HashMap<String, String>>> cardList = new ArrayList<>();
     SimpleAdapter simpleAdapter;
-    Button button;
+    Button button_add;
+    Button button_cancel;
+    Button button_delete;
+    Button button_save;
     EditText edit1;
     EditText edit2;
+    public int currentPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +46,18 @@ public class AddActivity extends AppCompatActivity {
         CardListView = (ListView)findViewById(R.id.lv1);
         edit1 = (EditText)findViewById(R.id.et_sidea);
         edit2 = (EditText)findViewById(R.id.et_sideb);
-        button = (Button)findViewById(R.id.btn_add);
+        button_add = (Button)findViewById(R.id.btn_add);
+        button_cancel = (Button)findViewById(R.id.btn_cancel);
+        button_delete = (Button)findViewById(R.id.btn_delete);
+        button_save = (Button)findViewById(R.id.btn_save);
+
 
         simpleAdapter = new SimpleAdapter(this, card, R.layout.list_card_item,
                 new String[] {"Question", "Answer"},
                 new int[]{R.id.tvquestion, R.id.tvanswer});
         CardListView.setAdapter(simpleAdapter);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button_add.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View v) {
@@ -66,9 +79,61 @@ public class AddActivity extends AppCompatActivity {
             }
         });
 
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                edit1.setText("");
+                edit2.setText("");
+                button_add.setVisibility(View.VISIBLE);
+                button_cancel.setVisibility(View.INVISIBLE);
+                button_delete.setVisibility(View.INVISIBLE);
+                button_save.setVisibility(View.GONE);
+
+            }
+        });
+
+        button_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                card.remove(currentPos);
+                simpleAdapter.notifyDataSetChanged();
+                edit1.setText("");
+                edit2.setText("");
+                button_add.setVisibility(View.VISIBLE);
+                button_cancel.setVisibility(View.INVISIBLE);
+                button_delete.setVisibility(View.INVISIBLE);
+                button_save.setVisibility(View.GONE);
+
+            }
+        });
+
+        button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                HashMap<String, String> carddata = new HashMap<>();
+                carddata.put("Question", edit1.getText().toString());
+                carddata.put("Answer", edit2.getText().toString());
+
+                card.set(currentPos, carddata);
+                simpleAdapter.notifyDataSetChanged();
+                edit1.setText("");
+                edit2.setText("");
+                button_add.setVisibility(View.VISIBLE);
+                button_cancel.setVisibility(View.INVISIBLE);
+                button_delete.setVisibility(View.INVISIBLE);
+                button_save.setVisibility(View.GONE);
+
+
+            }
+        });
+
         CardListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                currentPos = pos;
                 HashMap selectedItem = card.get(pos);
                 showEditCardDialog(selectedItem);
             }
@@ -80,6 +145,28 @@ public class AddActivity extends AppCompatActivity {
 
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_add_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_done:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+
     public void showEditCardDialog(final HashMap<String, String> mapdata) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
        // LayoutInflater inflater = this.getLayoutInflater();
@@ -91,6 +178,11 @@ public class AddActivity extends AppCompatActivity {
         dialogBuilder.setMessage(R.string.dialog_edit_card_body);
         dialogBuilder.setPositiveButton(R.string.dialog_edit_positive_button, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
+
+                button_add.setVisibility(View.GONE);
+                button_cancel.setVisibility(View.VISIBLE);
+                button_delete.setVisibility(View.VISIBLE);
+                button_save.setVisibility(View.VISIBLE);
                 edit1.setText(mapdata.get("Question"));
                 edit2.setText(mapdata.get("Answer"));
 
