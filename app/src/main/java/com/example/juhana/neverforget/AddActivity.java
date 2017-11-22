96ca609c5c2dada8135240f7b7e0d0c189a3d904
EljaVaisanen <elja_vaisanen@hotmail.com>
2017-11-22 11:08:52 +0200

    addActivityn korjaus. Vanhat cardlistit kannattaa poistaa

--- app/src/main/java/com/example/juhana/neverforget/AddActivity.java
+++ app/src/main/java/com/example/juhana/neverforget/AddActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -228,7 +229,7 @@ public class AddActivity extends AppCompatActivity {
     // insert cards to cardList
     private void saveCardsToCardList(){
         for (int i = 0; i < cards.size(); i++){
-            db.cardDao().InsertCards(new Card(cardListId, cards.get(i).get("question"), cards.get(i).get("answer")));
+            db.cardDao().InsertCards(new Card(cardListId, cards.get(i).get("Question"), cards.get(i).get("Answer")));
         }
     }
 }
